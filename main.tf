df1c2710317ecff0622011930de1e101992b0813
Nyerhovwo <101157174+NyerhovwoOnitcha@users.noreply.github.com>
2023-09-23 23:04:05 +0100

    #11 Install tf provider, create and destroy s3 bucket (#12)

--- main.tf
+++ main.tf
@@ -6,9 +6,12 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
-
 
 ## In the provider block we specify the configurations for the provider we chose. It's we optional
 
@@ -17,11 +20,26 @@ provider "random" {
 }
 
 
+provider "aws" {
+  # Configuration options
+}
+
+
+
 resource "random_string" "bucket_name" {
-  length           = 16
+  lower = true
+  upper = false
+  length  = 16
+  special = false
   
 }
 
-output "random_bucket_name" {
-    value = random_string.bucket_name.id
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+ 
 }
+
+output "random_bucket_name" {
+    value = random_string.bucket_name.result
+}
+
