85a78d4914ce36827adf92a470492c2ec843870d
Nyerhovwo <101157174+NyerhovwoOnitcha@users.noreply.github.com>
2023-09-26 00:20:07 +0100

    #25 refactor s3 bucket into the new terrahouse_aws module (#26)

--- variables.tf
+++ variables.tf
@@ -1,11 +1,6 @@
 variable "user_uuid" {
   type        = string
   description = "The UUID of the user"
-   
-  validation {
-    condition     = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.user_uuid))
-    error_message = "The user_uuid must be in the format of a UUID"
-  }
 }
 
 variable "bucket_name" {
