3006452e98d78cbab1a6e0fab2f8a9325fafc65f
Nyerhovwo Onitcha <pharoahpaul1@gmail.com>
2023-09-26 03:06:39 +0000

    #27 enabled static website hosting, uploaded index.html

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-
+# Create s3 Bucket
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
 
@@ -17,3 +17,37 @@ resource "aws_s3_bucket" "website_bucket" {
   }
  
 }
+
+# Enable s3 static website hosting on s3 bucket created above
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+
+# Upload index.html file to bucket above
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html" 
+  source = var.index_html_filepath
+  
+  etag = filemd5(var.index_html_filepath)
+}
+
+# Upload error.html file to bucket above
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html" 
+  source = var.error_html_filepath
+
+  etag = filemd5(var.error_html_filepath)
+}
+
--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "bucket_name" {
   type        = string
  
 }
+
+variable "index_html_filepath" {
+  type        = string
+  
+}
+
+variable "error_html_filepath" {
+  type        = string
+  
+}
