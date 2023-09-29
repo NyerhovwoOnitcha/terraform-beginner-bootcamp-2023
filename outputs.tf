e650fb13b7a0a23da0f816a2b0fa8b9509b07461
Nyerhovwo <101157174+NyerhovwoOnitcha@users.noreply.github.com>
2023-09-29 10:31:47 +0100

    31 invalidate cloudfront distribution (#32)
    * #31 Invalidate cloudfront cache whwn content changes

--- outputs.tf
+++ outputs.tf
@@ -19,3 +19,8 @@ output "bucket_name" {
 output "website_endpoint" {
     value = module.terrahouse_aws.s3_website_endpoint
 }
+
+output "cloudfront_url" {
+    description = "Cloudfront Distribution Domain Name"
+    value = module.terrahouse_aws.cloudfront_url
+} 
