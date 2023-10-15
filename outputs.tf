ffddb11567fcb998e6ef682f6c7a696411f94bbc
Nyerhovwo <101157174+NyerhovwoOnitcha@users.noreply.github.com>
2023-10-15 21:01:32 +0100

    37 terratowns provider (#39)
    * #37 setting up skeleton for creating tf custom provider
    
    * #37 implement terraform provider block without errors

--- outputs.tf
+++ outputs.tf
@@ -11,16 +11,19 @@
 #     value = aws_s3_bucket.website_bucket
 # }
 
+
+
+
 ### This outputs the bucket name only, it's an attribute os the s3 resource
-output "bucket_name" {
-    value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#     value = module.terrahouse_aws.bucket_name
+# }
 
-output "website_endpoint" {
-    value = module.terrahouse_aws.s3_website_endpoint
-}
+# output "website_endpoint" {
+#     value = module.terrahouse_aws.s3_website_endpoint
+# }
 
-output "cloudfront_url" {
-    description = "Cloudfront Distribution Domain Name"
-    value = module.terrahouse_aws.cloudfront_url
-} 
+# output "cloudfront_url" {
+#     description = "Cloudfront Distribution Domain Name"
+#     value = module.terrahouse_aws.cloudfront_url
+# } 
